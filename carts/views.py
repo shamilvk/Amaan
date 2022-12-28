58bc27fc83a9ea481aa025cc7d0d17da5380187f
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2022-12-28 21:28:17 +0530

    Add to Cart using Session Keys, Incrementdecrementremove Cart Items

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.http import HttpResponse
 
@@ -11,8 +12,6 @@ def cart_id(request):
     if not cart:
         cart = request.session.create()
     return cart
-
-
 
 
 def add_cart(request, product_id):
@@ -36,9 +35,48 @@ def add_cart(request, product_id):
             cart = cart,          
         )
         cart_item.save()
-    return HttpResponse(cart_item.quantity)
-    exit()
     return redirect('cart')
 
-def cart(request):
-    return render(request, 'store/cart.html')
+
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (3 * total)/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass #just ignore
+    
+    context = {
+        'total' : total,
+        'quantity' : quantity,
+        'cart_items' : cart_items,
+        'tax' : tax,
+        'grand_total' : grand_total,
+    }
+        
+    return render(request, 'store/cart.html', context)
