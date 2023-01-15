ba2148ee79a3c5eef955603ee3b689f448825328
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-01-15 20:56:45 +0530

    Cart Checkout, automatically assign the Cart Items to Logged-in

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from .models import Accounts
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+
 #verification email
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
@@ -11,6 +12,11 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+
+from carts.views import cart_id
+from carts.models import Cart,CartItem
+import requests
+
 from django.http import HttpResponse
 
 
@@ -64,9 +70,58 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
         
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    
+                    #Getting the producat variations by cart id 
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                        
+                    # Get the cart items from the user to acces his product variation
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variations = item.variations.all()
+                        ex_var_list.append(list(existing_variations))
+                        id.append(item.id)
+                        
+                        
+                    # product_variation = [1, 2, 3, 4, 6]
+                    # ex_var_list = [4, 6, 3, 5] 
+                    for pv in  product_variation:
+                        if pv in ex_var_list:
+                            index = ex_var_list.index(pv)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)  
+                            item.quantity += 1
+                            item.user = user
+                            item.save()  
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                  pass
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+              
+                #next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from store.models import Product, Variation
+from accounts.models import Accounts
 
 # Create your models here.
 
@@ -12,11 +13,12 @@ class Cart(models.Model):
     
     
 class CartItem(models.Model):
-    product   = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user       = models.ForeignKey(Accounts, on_delete=models.CASCADE, null=True )
+    product    = models.ForeignKey(Product, on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation, blank=True)
-    cart      = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    quantity  = models.IntegerField()
-    is_active = models.BooleanField(default=True)
+    cart       = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    quantity   = models.IntegerField()
+    is_active  = models.BooleanField(default=True)
     
     def sub_total(self):
         return self.product.price * self.quantity
