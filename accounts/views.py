0ff1b8561499a7221ac0f9683bb7027a7b4287da
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-01-25 21:56:47 +0530

    Dashboard Edits

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Accounts
+from orders.models import Order
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
@@ -152,7 +153,12 @@ def activate(request ,uidb64, token):
     
 @login_required(login_url = 'login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+    context = {
+        'orders_count' : orders_count,
+    }
+    return render(request, 'accounts/dashboard.html',context)
 
 
 def forgotPassword(request):
