62d74666df96c87610debc86c04d13e90744dfa5
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-01-25 21:04:27 +0530

    Rating Average Stars & Review Counter

--- store/models.py
+++ store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from category.models import Category
 from django.urls import reverse
 from accounts.models import Accounts
-from django.db.models import Avg
+from django.db.models import Avg, Count
 
 # Create your models here.
 
@@ -32,6 +32,15 @@ class Product(models.Model):
         if reviews['average'] is not None:
             avg = float(reviews['average'])
         return avg
+    
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count =0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
+        
+
  
 class VariationManager(models.Manager):
     def colors(self):
