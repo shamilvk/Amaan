cef78846df0e3cfde84c3c33749ddbebdc71d543
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-01-25 16:34:03 +0530

    Store the Rating & Reviews last commit

--- store/forms.py
+++ store/forms.py
@@ -4,4 +4,4 @@ from .models import ReviewRating
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = ReviewRating
-        fields = ['subject', 'review', 'ratin']
+        fields = ['subject', 'review', 'rating']
