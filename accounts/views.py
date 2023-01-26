ebff48c08b60cd766e75458de0528d8afefa46b4
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-01-27 01:10:52 +0530

    Change Your Password Fucntionality

--- accounts/views.py
+++ accounts/views.py
@@ -225,7 +225,7 @@ def resetPassword(request):
     else:
         return render(request, 'accounts/resetPassword.html')
     
-
+@login_required(login_url = 'login')
 def my_orders(request):
     orders = Order.objects.filter(user=request.user, is_ordered=True)
     context = {
@@ -234,6 +234,7 @@ def my_orders(request):
     
     return render(request, 'accounts/my_orders.html',context)
 
+@login_required(login_url = 'login')
 def edit_profile(request):
     userprofile = get_object_or_404(UserProfile, user=request.user)
     if request.method == 'POST':
@@ -254,4 +255,30 @@ def edit_profile(request):
         
     }
     return render(request, 'accounts/edit_profile.html',context)
+
+@login_required(login_url = 'login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+        
+        user = Accounts.objects.get(username__exact=request.user.username)
+        
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                # auth.logout(request)
+                messages.success(request, 'Your password has been updated')
+                return redirect('change_password')
+            else:
+                messages.error(request, 'Please enter valid current password')
+                return redirect('change_password')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('change_password')
+    return render(request, 'accounts/change_password.html')
+
 
