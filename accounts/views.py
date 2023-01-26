5957b117440e3f5c1020b115b1636009c4f6bfdc
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-01-26 22:31:51 +0530

    Edit Profile Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-from .forms import RegistrationForm
-from .models import Accounts
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import RegistrationForm, UserForm, UserProfileForm
+from .models import Accounts, UserProfile
 from orders.models import Order
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
@@ -235,5 +235,23 @@ def my_orders(request):
     return render(request, 'accounts/my_orders.html',context)
 
 def edit_profile(request):
-    return render(request, 'accounts/edit_profile.html')
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile has been updated')
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+    context = {
+        'user_form' : user_form,
+        'profile_form' :profile_form,
+        'userprofile' : userprofile,
+        
+    }
+    return render(request, 'accounts/edit_profile.html',context)
 
