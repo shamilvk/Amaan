436f2a9369540df5707c11ddcbd2e2eefa2d1c84
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-01-27 19:20:04 +0530

    normal commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,8 @@ from django.utils.html import format_html
 
 # Register your models here.
 
+
+
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
